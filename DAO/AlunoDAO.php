ceaacf5301a732d56c91c47ad4e0893a5f963d4a
BrunoFreitas09 <brunootaviocostadefreitas@gmail.com>
2025-04-07 15:45:12 -0300

    Atualizacoes no projeto

--- DAO/AlunoDAO.php
+++ DAO/AlunoDAO.php
@@ -50,8 +50,38 @@
             $sql="SELECT * FROM alunos WHERE id=?";
             $stmt = parent::$conexao->prepare($sql);
             $stmt->bindValue(1, $id);
+            $stmt->execute();
 
+            return $stmt->fetchObject("App\Model\Aluno")
+        }
+        /**
+         * metódo que retorna todos os registros da tabela pessoa no 
+         * banco de dados
+         */
+        public function select() : array 
+        {
+            $sql = "SELECT * FROM aluno ";
+
+            $stmt = parent::$conexao->prepare($sql);
+            $stmt->execute();
+
+            //retorna um array com as linhas retornadas da consulta.Observe que 
+            //o Array é um array de objetos. Os objetos são dos tipos de um stdClass e 
+            //foram criados automaticamente pelo método FetchAlldo PDO
+            return $stmt->fethcAll(DAO:: FETCH_CLASS, "App\Model\Aluno") 
+        }    
+        /**
+         * Remove um registro da tabela pessoa do banco de dados.
+         * Note que o metódo exige um parâmentro $id do tipo inteiro 
+         */
+        public function delete(int $id) : bool
+        {
+            $sql = "DELETE FROM aluno WHERE id=? ";
+
+            $stmt = parent::$conexao->($sql);
+            $stmt->bindValue(1, $id);
+            return $stmt->execute();
         }
     }
-    //quem nasceu pra ser patrao
+
 ?>
