d67bfbc8c06f3b2291d03a24d804858ee4b8ac48
BrunoFreitas09 <brunootaviocostadefreitas@gmail.com>
2025-08-14 18:06:17 -0300

    arrumando o DAO

--- DAO/AlunoDAO.php
+++ DAO/AlunoDAO.php
@@ -1,87 +1,77 @@
 <?php
-    namespace App\DAO;
 
-    use App\Model\Aluno;
+namespace App\DAO;
 
-    final class AlunoDAO extends DAO
+use App\Model\Aluno;
+
+final class AlunoDAO extends DAO
+{
+    public function __construct()
     {
-        public function __construct()
-        {
-            parent::__construct();
-        }
-
-        public function save (Aluno $model): Aluno
-        {
-            return ($model->Id == null) ? $this->insert($model) :
-            $this->update($model);
-        }
-
-        public function insert (Aluno $model): Aluno
-        {
-            $sql = "INSERT INTO aluno (nome, ra, curso) VALUES (?, ?, ?)";
-
-            $stmt = parent::$conexao->prepare($sql);
-
-            $stmt->bindValue(1, $model->Nome);
-            $stmt->bindValue(2, $model->RA);
-            $stmt->bindValue(3, $model->Curso);
-            $stmt->Execute();
-            $model->Id = parent::$conexao->lastInsertId();
-
-            return $model;
-        }
-
-        public function update(Aluno $model): Aluno
-        {
-             $sql = "UPDATE aluno SET nome=?, ra=?, curso=? WHERE id=?";
-
-             $stmt = parent::$conexao->prepare($sql);
-             $stmt->bindValue(1, $model->Nome);
-             $stmt->bindValue(2, $model->RA);
-             $stmt->bindValue(3, $model->Curso);
-             $stmt->bindValue(4, $model->Id);
-             $stmt->Execute();
-
-             return $model;
-        }
-
-        public function selectById(int $id) : ?Aluno
-        {
-            $sql="SELECT * FROM alunos WHERE id=?";
-            $stmt = parent::$conexao->prepare($sql);
-            $stmt->bindValue(1, $id);
-            $stmt->execute();
-
-            return $stmt->fetchObject("App\Model\Aluno")
-        }
-        /**
-         * metódo que retorna todos os registros da tabela pessoa no 
-         * banco de dados
-         */
-        public function select() : array 
-        {
-            $sql = "SELECT * FROM aluno ";
-
-            $stmt = parent::$conexao->prepare($sql);
-            $stmt->execute();
-
-            //retorna um array com as linhas retornadas da consulta.Observe que 
-            //o Array é um array de objetos. Os objetos são dos tipos de um stdClass e 
-            //foram criados automaticamente pelo método FetchAlldo PDO
-            return $stmt->fethcAll(DAO:: FETCH_CLASS, "App\Model\Aluno") 
-        }    
-        /**
-         * Remove um registro da tabela pessoa do banco de dados.
-         * Note que o metódo exige um parâmentro $id do tipo inteiro 
-         */
-        public function delete(int $id) : bool
-        {
-            $sql = "DELETE FROM aluno WHERE id=? ";
-
-            $stmt = parent::$conexao->($sql);
-            $stmt->bindValue(1, $id);
-            return $stmt->execute();
-        }
+        parent::__construct();
     }
 
-?>
+    public function save(Aluno $model) : Aluno
+    {
+        return ($model->Id == null) ? $this->insert($model) : 
+            $this->update($model);
+    }
+
+    public function insert(Aluno $model) : Aluno
+    {
+        $sql = "INSERT INTO aluno (nome, ra, curso) VALUES (?, ?, ?) ";
+
+        $stmt = parent::$conexao->prepare($sql);        
+        $stmt->bindValue(1, $model->Nome);
+        $stmt->bindValue(2, $model->RA);
+        $stmt->bindValue(3, $model->Curso);
+        $stmt->execute();
+        $model->Id = parent::$conexao->lastInsertId();
+        
+        return $model;
+    }
+
+    public function update(Aluno $model) : Aluno
+    {
+        $sql = "UPDATE aluno SET nome=?, ra=?, curso=? WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);
+        $stmt->bindValue(1, $model->Nome);
+        $stmt->bindValue(2, $model->RA);
+        $stmt->bindValue(3, $model->Curso);
+        $stmt->bindValue(4, $model->Id);
+        $stmt->execute();
+        
+        return $model;
+    }
+
+    public function selectById(int $id) : ?Aluno
+    {
+        $sql = "SELECT * FROM aluno WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+
+        return $stmt->fetchObject("App\Model\Aluno");
+    }
+
+
+    public function select() : array
+    {
+        $sql = "SELECT * FROM aluno ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->execute();
+        return $stmt->fetchAll(DAO::FETCH_CLASS, "App\Model\Aluno");
+    }
+
+    public function delete(int $id) : bool
+    {
+        $sql = "DELETE FROM aluno WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->bindValue(1, $id);
+        return $stmt->execute();
+    }
+}
--- DAO/CategoriaDAO.php
+++ DAO/CategoriaDAO.php
@@ -1,84 +1,85 @@
 <?php
-    namespace App\DAO;
 
-    use App\Model\Categoria;
+namespace App\DAO;
 
-    final class CategoriaDAO extends DAO
+use App\Model\Categoria;
+
+
+final class CategoriaDAO extends DAO
+{
+
+    public function __construct()
     {
-        public function __construct()
-        {
-            parent::__construct();
-        }
 
-        public function save (Categoria $model): Categoria
-        {
-            return ($model->Id == null) ? $this->insert($model) :
-            $this->update($model);
-        }
-
-        public function insert (Categoria $model): Categoria
-        {
-            $sql = "INSERT INTO Categoria (descricao) VALUES (?)";
-
-            $stmt = parent::$conexao->prepare($sql);
-
-            $stmt->bindValue(1, $model->Descricao);
-
-            $stmt->Execute();
-            $model->Id = parent::$conexao->lastInsertId();
-
-            return $model;
-        }
-
-        public function update(Categoria $model): Categoria
-        {
-             $sql = "UPDATE Categoria SET descricao=? WHERE id=?";
-
-             $stmt = parent::$conexao->prepare($sql);
-             $stmt->bindValue(1, $model->Descricao);
-             $stmt->bindValue(4, $model->Id);
-             $stmt->Execute();
-
-             return $model;
-        }
-
-        public function selectById(int $id) : ?Categoria
-        {
-            $sql="SELECT * FROM Categorias WHERE id=?";
-            $stmt = parent::$conexao->prepare($sql);
-            $stmt->bindValue(1, $id);
-            $stmt->execute();
-
-            return $stmt->fetchObject("App\Model\Categoria")
-        }
-        /**
-         * metódo que retorna todos os registros da tabela pessoa no 
-         * banco de dados
-         */
-        public function select() : array 
-        {
-            $sql = "SELECT * FROM Categoria ";
-
-            $stmt = parent::$conexao->prepare($sql);
-            $stmt->execute();
-
-            //retorna um array com as linhas retornadas da consulta.Observe que 
-            //o Array é um array de objetos. Os objetos são dos tipos de um stdClass e 
-            //foram criados automaticamente pelo método FetchAlldo PDO
-            return $stmt->fethcAll(DAO:: FETCH_CLASS, "App\Model\Categoria") 
-        }    
-        /**
-         * Remove um registro da tabela pessoa do banco de dados.
-         * Note que o metódo exige um parâmentro $id do tipo inteiro 
-         */
-        public function delete(int $id) : bool
-        {
-            $sql = "DELETE FROM Categoria WHERE id=? ";
-
-            $stmt = parent::$conexao->($sql);
-            $stmt->bindValue(1, $id);
-            return $stmt->execute();
-        }
+        parent::__construct();
     }
 
-?>
+    public function save(Categoria $model) : Categoria
+    {
+
+        return ($model->Id == null) ? $this->insert($model) : $this->update($model);
+    }
+
+
+
+    public function insert(Categoria $model) : Categoria
+    {
+        $sql = "INSERT INTO categoria (descricao) VALUES (?) ";
+
+        $stmt = parent::$conexao->prepare($sql);
+        
+        $stmt->bindValue(1, $model->Descricao);
+
+        $stmt->execute();
+
+        $model->Id = parent::$conexao->lastInsertId();
+        
+        return $model;
+    }
+
+
+    public function update(Categoria $model) : Categoria
+    {
+        $sql = "UPDATE categoria SET descricao=? WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);
+        $stmt->bindValue(1, $model->Descricao);
+        $stmt->bindValue(2, $model->Id);
+        $stmt->execute();
+        
+        return $model;
+    }
+
+
+    public function selectById(int $id) : ?Categoria
+    {
+        $sql = "SELECT * FROM categoria WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+
+        return $stmt->fetchObject("App\Model\Categoria");
+    }
+
+
+    public function select() : array
+    {
+        $sql = "SELECT * FROM categoria ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->execute();
+
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS, "App\Model\Categoria");
+    }
+
+    public function delete(int $id) : bool
+    {
+        $sql = "DELETE FROM categoria WHERE id=? ";
+
+        $stmt = parent::$conexao->prepare($sql);  
+        $stmt->bindValue(1, $id);
+        return $stmt->execute();
+    }
+}
